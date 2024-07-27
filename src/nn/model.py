e1e2758a7b4580e317a0d6bd8e1557a425eb2be5
lorenzo-cm <lorenzocarneirobr@gmail.com>
2024-07-27 18:59:37 -0300

    plot graphs

--- src/nn/model.py
+++ src/nn/model.py
@@ -41,7 +41,7 @@ class CaptchaClassifierBigger(nn.Module):
         x = self.pool(self.relu(self.bn1(self.conv1(x))))
         x = self.pool(self.relu(self.bn2(self.conv2(x))))
         x = self.pool(self.relu(self.bn3(self.conv3(x))))
-        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = x.view(x.size(0), -1)
         x = self.dropout(self.relu(self.fc1(x)))
         x = self.fc2(x)
         return x
